48eaac003ce6d8cbf3a0a81b4020303854b611b1
Shubham Maurya <157043197+Skmaurya05550@users.noreply.github.com>
2024-10-12 22:54:43 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+
+    <script>
+        
+        var u=0;
+setInterval (()=>{ function g(){
+
+u+=5
+            document.body.style.transform = ` perspective(900px) rotateY(${u}deg)`
+           
+            
+            }setInterval (g,50)
+            
+            },1000)
+        
+        
+    </script>
